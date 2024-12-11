669d261cdde4e78a1f8a5bc98800cca720b2b3b2
nick-36 <makwananikhil36@gmail.com>
2024-12-11 18:46:04 +0530

    feature/animated-card

--- src/app/(dashboard)/forms/[id]/page.tsx
+++ src/app/(dashboard)/forms/[id]/page.tsx
@@ -44,7 +44,7 @@ const FormDetailsPage = async ({
           title="Total visits"
           statValue={form?.visits.toLocaleString() ?? ""}
           description="all time visits"
-          className="shadow-md shadow-blue-600"
+          className="shadow-md shadow-blue-600 [background:linear-gradient(45deg,#000,#000)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.blue.400/.48)_80%,_theme(colors.blue.500)_86%,_theme(colors.blue.300)_0%,_theme(colors.blue.500)_94%,_theme(colors.blue.600/.48))_border-box]"
           loading={false}
           icon={<LuView className="text-blue-500" />}
         />
@@ -52,7 +52,7 @@ const FormDetailsPage = async ({
           title="Total Submission"
           statValue={form?.submissions.toLocaleString() ?? ""}
           description="all time submissions"
-          className="shadow-md shadow-yellow-600"
+          className="shadow-md shadow-yellow-600 [background:linear-gradient(45deg,#000,#000)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.yellow.400/.48)_80%,_theme(colors.yellow.500)_86%,_theme(colors.yellow.300)_0%,_theme(colors.yellow.500)_94%,_theme(colors.yellow.600/.48))_border-box]"
           loading={false}
           icon={<BookText className="text-yellow-500" />}
         />
@@ -60,7 +60,7 @@ const FormDetailsPage = async ({
           title="Submission Rate"
           statValue={`${form?.submissionRate.toLocaleString() ?? ""}%`}
           description="all the visits that results into submissions"
-          className="shadow-md shadow-green-600"
+          className="shadow-md shadow-green-600 [background:linear-gradient(45deg,#000,#000)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.green.400/.48)_80%,_theme(colors.green.500)_86%,_theme(colors.green.300)_0%,_theme(colors.green.500)_94%,_theme(colors.green.600/.48))_border-box]"
           loading={false}
           icon={<MousePointerClick className="text-green-500" />}
         />
@@ -68,7 +68,7 @@ const FormDetailsPage = async ({
           title="Bounce Rate"
           statValue={`${form?.bounceRate.toLocaleString() ?? ""}%`}
           description="all the visits that left without interacting"
-          className="shadow-md shadow-red-600"
+          className="shadow-md shadow-red-600 [background:linear-gradient(45deg,#000,#000)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.red.400/.48)_80%,_theme(colors.red.500)_86%,_theme(colors.red.300)_0%,_theme(colors.red.500)_94%,_theme(colors.red.600/.48))_border-box]"
           loading={false}
           icon={<PointerOff className="text-red-500" />}
         />
