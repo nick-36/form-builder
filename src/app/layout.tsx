372aae2272c78c98e1494452ddb45ab6ef7c71a4
nick-36 <makwananikhil36@gmail.com>
2025-01-11 21:20:31 +0530

    db url change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
 }) {
   return (
     <>
-      {/* @ts-expect-error Server Component */}
       <ClerkProvider>
         <html lang="en">
           <body className={cn(poppins.className)}>
